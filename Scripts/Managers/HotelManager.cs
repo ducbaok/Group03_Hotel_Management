7204d8546f3c6cb62a70933d74a94e7473340773
Yunasawa <yunasawa100@gmail.com>
2025-04-24 17:43:44 +0700

    [WIP] Add managers

---
+++ Scripts/Managers/HotelManager.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelReservation
+{
+    public class HotelManager
+    {
+        public Dictionary<UID, Hotel> Hotels = new();
+
+        public async Task<List<UID>> SearchHotelAsync(List<UID> hotels, string query)
+        {
+            Func<List<UID>> task = () =>
+                Hotels.Values.ToList().Where(h =>
+                       h.Description.Name.Contains(query, StringComparison.OrdinalIgnoreCase)
+                    || h.Description.Address.Contains(query, StringComparison.OrdinalIgnoreCase))
+                .Select(h => h.ID)
+                .ToList();
+
+            return await Task.Run(task);
+        }
+    }
+}
