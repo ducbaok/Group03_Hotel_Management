4d8c362e3dc4368ed9f464c746e4ccc35183161e
Yunasawa <yunasawa100@gmail.com>
2025-04-25 00:38:20 +0700

    [RLS] Add sign up validation

--- Scripts/Managers/AccountManager.cs
+++ Scripts/Managers/AccountManager.cs
@@ -2,39 +2,42 @@
 {
     public class AccountManager
     {
-        public Account Account = new();
+        public UID AccountID = new();
+        public List<Account>? Accounts;
 
-        public void SignUpAccount(AccountVerificationType type, string account, string password)
+        public void SignUpAccount(AccountVerificationType type, string input, string password)
         {
+            Account? account;
+
             if (type == AccountVerificationType.Email)
             {
-                if (!Handler.Validator.ValidateEmail(account))
+                if (!Handler.Validator.ValidateEmail(input))
                 {
                     Event.OnAccountVerificated?.Invoke(AccountVerificationResult.SignUpEmailNotValid);
                     return;
                 }
-                if (true) // Check if email has existed in database
+                if ((account = Accounts?.Find(i => i.Email == input)) == null)
                 {
                     Event.OnAccountVerificated?.Invoke(AccountVerificationResult.SignUpEmailHasExisted);
                     return;
                 }
 
-                // Try get account ID from database
+                Console.WriteLine($"Account ID: {AccountID = account.ID}");
             }
             else if (type == AccountVerificationType.PhoneNumber)
             {
-                if (!Handler.Validator.ValidatePhoneNumber(account))
+                if (!Handler.Validator.ValidatePhoneNumber(input))
                 {
                     Event.OnAccountVerificated?.Invoke(AccountVerificationResult.SignUpPhoneNumberNotValid);
                     return;
                 }
-                if (true) // Check if phone number has existed in database
+                if ((account = Accounts?.Find(i => i.PhoneNumber == input)) == null)
                 {
                     Event.OnAccountVerificated?.Invoke(AccountVerificationResult.SignUpPhoneNumberHasExisted);
                     return;
                 }
 
-                // Try get account ID from database
+                Console.WriteLine($"Account ID: {AccountID = account.ID}");
             }
         }
 
