2a1df467e8397583e117c458542acc9eec4ae7a5
Khong Minh Cuong <cuong.km225603@sis.hust.edu.vn>
2025-04-24 21:59:27 +0700

    [WIP] feat: add random data generators for account and room description

--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace HotelReservation
+﻿using HotelReservation.Utils;
+
+namespace HotelReservation
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
             System.Console.WriteLine("Hello, World!");
+            // Account
+            List<Account> testAccounts = DataGenerator.GenerateAccountsInRange(1, 20, true);
+            foreach (var acc in testAccounts){
+                Console.WriteLine($"ID={(uint)acc.ID}, Name={acc.Name}, Email={acc.Email}, Phone={acc.Phone}, Password={acc.Password}");
+            }
+            
+            // RoomDescription
+            DataGenerator.PrintRoomDescription(DataGenerator.GenerateRandomRoomDescription());
         }
     }
 }
