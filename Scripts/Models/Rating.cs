4f774e73c4056c89c825f2a525530cb51779634c
Yunasawa <yunasawa100@gmail.com>
2025-04-28 12:56:26 +0700

    [WIP] Modeling

---
+++ Scripts/Models/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelReservation
+{
+    public struct Rating
+    {
+        private float _rating;
+
+        public static implicit operator float(Rating rating) => rating._rating;
+        public static implicit operator Rating(float rating) => new() { _rating = rating };
+    }
+}
