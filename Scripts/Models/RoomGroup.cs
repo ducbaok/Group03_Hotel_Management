b0750bf3971ab2db941341497e2dae185343d566
Yunasawa <yunasawa100@gmail.com>
2025-04-28 12:57:31 +0700

    [WIP]

---
+++ Scripts/Models/RoomGroup.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelReservation
+{
+    public class RoomCategorization
+    {
+        public Dictionary<UID, RoomGroup> Rooms { get; set; } = new(); // Key is ID of RoomGroup
+    }
+
+    public class RoomGroup
+    {
+        public string Name { get; set; } = string.Empty;
+        public RoomDescription Description { get; set; } = new();
+        public Dictionary<ushort, RoomStatus> Rooms { get; set; } = new();
+    }
+
+    public class RoomStatus
+    {
+        public bool IsAvailable { get; set; } = true;
+    }
+}
