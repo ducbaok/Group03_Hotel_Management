0e2e8cfbea7fd4de5cef6fb60d49aa8bed2e9957
Yunasawa <yunasawa100@gmail.com>
2025-05-05 15:54:28 +0700

    [REF] Change project platform to .NET MAUI App

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Reservation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Hotel_Reservation;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
